a59d4b7abe337f0b10b9ea8a809a44521b8f44e5
rhrocha <rhrocha>
2018-10-01 16:29:37 -0300

    Added EnumUtils class.

---
+++ src/WDNUtils.Common/Common/EnumUtils.cs
@@ -0,0 +1,272 @@
+﻿using System;
+
+namespace WDNUtils.Common
+{
+    /// <summary>
+    /// Enum extensions
+    /// </summary>
+    public static class EnumUtils
+    {
+        /// <summary>
+        /// Retrieves an array of the values of the constants in a specified enumeration
+        /// </summary>
+        /// <typeparam name="T">An enumeration type</typeparam>
+        /// <returns>An array that contains the values of the constants in enumType</returns>
+        /// <exception cref="ArgumentException">T is not an System.Enum</exception>
+        /// <exception cref="InvalidOperationException">The method is invoked by reflection in a reflection-only context, -or- T is a type from an assembly loaded in a reflection-only context</exception>
+        public static T[] GetValues<T>()
+        {
+            return (T[])Enum.GetValues(typeof(T));
+        }
+
+        /// <summary>
+        /// Determines whether one or more bit fields are set in an enum value
+        /// </summary>
+        /// <param name="value">An enumeration value</param>
+        /// <param name="flags">The combinations of bit fields to compare with the value</param>
+        /// <returns>true if one of the combination of bit fields are set in the specified value; otherwise, false</returns>
+        /// <exception cref="ArgumentException">flag is a different type than the current instance</exception>
+        public static bool HasAnyFlag(this Enum value, params Enum[] flags)
+        {
+            foreach (var item in flags)
+            {
+                if (value.HasFlag(item))
+                    return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Determines whether any of the specified bits are set in an enum value
+        /// </summary>
+        /// <param name="value">An enumeration value</param>
+        /// <param name="bitMask">The bits to compare with the value</param>
+        /// <returns>true if one of the specified bits are set in the value; otherwise, false</returns>
+        /// <exception cref="ArgumentException">flag is a different type than the current instance</exception>
+        public static bool HasAnyBit(this Enum value, Enum bitMask)
+        {
+            if (!value.GetType().Equals(bitMask.GetType()))
+                throw new InvalidOperationException();
+
+            switch (value.GetTypeCode())
+            {
+                case TypeCode.SByte:
+                case TypeCode.Int16:
+                case TypeCode.Int32:
+                case TypeCode.Int64:
+                    return (Convert.ToInt64(value) & Convert.ToInt64(bitMask)) != 0;
+
+                case TypeCode.Byte:
+                case TypeCode.UInt16:
+                case TypeCode.UInt32:
+                case TypeCode.UInt64:
+                    return (Convert.ToUInt64(value) & Convert.ToUInt64(bitMask)) != 0;
+
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+
+        /// <summary>
+        /// Converts the specified nullable 8-bit unsigned integer to an enumeration member
+        /// </summary>
+        /// <typeparam name="T">The enumeration type to return</typeparam>
+        /// <param name="value">The value to convert to an enumeration member</param>
+        /// <param name="nullValue">The enum to be returned if the value is null</param>
+        /// <returns>The value converted to the enumeration; or nullValue if the value is null</returns>
+        /// <exception cref="ArgumentException">T is not an enum type</exception>
+        /// <exception cref="ArgumentOutOfRangeException">The value is not defined in the T enumeration</exception>
+        public static T ConvertEnum<T>(byte? value, T nullValue)
+        {
+            return (!value.HasValue) ? nullValue : ConvertEnum<T>(value.Value);
+        }
+
+        /// <summary>
+        /// Converts the specified nullable 8-bit signed integer to an enumeration member
+        /// </summary>
+        /// <typeparam name="T">The enumeration type to return</typeparam>
+        /// <param name="value">The value to convert to an enumeration member</param>
+        /// <param name="nullValue">The enum to be returned if the value is null</param>
+        /// <returns>The value converted to the enumeration; or nullValue if the value is null</returns>
+        /// <exception cref="ArgumentException">T is not an enum type</exception>
+        /// <exception cref="ArgumentOutOfRangeException">The value is not defined in the T enumeration</exception>
+        public static T ConvertEnum<T>(sbyte? value, T nullValue)
+        {
+            return (!value.HasValue) ? nullValue : ConvertEnum<T>(value.Value);
+        }
+
+        /// <summary>
+        /// Converts the specified nullable 16-bit signed integer to an enumeration member
+        /// </summary>
+        /// <typeparam name="T">The enumeration type to return</typeparam>
+        /// <param name="value">The value to convert to an enumeration member</param>
+        /// <param name="nullValue">The enum to be returned if the value is null</param>
+        /// <returns>The value converted to the enumeration; or nullValue if the value is null</returns>
+        /// <exception cref="ArgumentException">T is not an enum type</exception>
+        /// <exception cref="ArgumentOutOfRangeException">The value is not defined in the T enumeration</exception>
+        public static T ConvertEnum<T>(short? value, T nullValue)
+        {
+            return (!value.HasValue) ? nullValue : ConvertEnum<T>(value.Value);
+        }
+
+        /// <summary>
+        /// Converts the specified nullable 16-bit unsigned integer to an enumeration member
+        /// </summary>
+        /// <typeparam name="T">The enumeration type to return</typeparam>
+        /// <param name="value">The value to convert to an enumeration member</param>
+        /// <param name="nullValue">The enum to be returned if the value is null</param>
+        /// <returns>The value converted to the enumeration; or nullValue if the value is null</returns>
+        /// <exception cref="ArgumentException">T is not an enum type</exception>
+        /// <exception cref="ArgumentOutOfRangeException">The value is not defined in the T enumeration</exception>
+        public static T ConvertEnum<T>(ushort? value, T nullValue)
+        {
+            return (!value.HasValue) ? nullValue : ConvertEnum<T>(value.Value);
+        }
+
+        /// <summary>
+        /// Converts the specified nullable 32-bit signed integer to an enumeration member
+        /// </summary>
+        /// <typeparam name="T">The enumeration type to return</typeparam>
+        /// <param name="value">The value to convert to an enumeration member</param>
+        /// <param name="nullValue">The enum to be returned if the value is null</param>
+        /// <returns>The value converted to the enumeration; or nullValue if the value is null</returns>
+        /// <exception cref="ArgumentException">T is not an enum type</exception>
+        /// <exception cref="ArgumentOutOfRangeException">The value is not defined in the T enumeration</exception>
+        public static T ConvertEnum<T>(int? value, T nullValue)
+        {
+            return (!value.HasValue) ? nullValue : ConvertEnum<T>(value.Value);
+        }
+
+        /// <summary>
+        /// Converts the specified nullable 32-bit unsigned integer to an enumeration member
+        /// </summary>
+        /// <typeparam name="T">The enumeration type to return</typeparam>
+        /// <param name="value">The value to convert to an enumeration member</param>
+        /// <param name="nullValue">The enum to be returned if the value is null</param>
+        /// <returns>The value converted to the enumeration; or nullValue if the value is null</returns>
+        /// <exception cref="ArgumentException">T is not an enum type</exception>
+        /// <exception cref="ArgumentOutOfRangeException">The value is not defined in the T enumeration</exception>
+        public static T ConvertEnum<T>(uint? value, T nullValue)
+        {
+            return (!value.HasValue) ? nullValue : ConvertEnum<T>(value.Value);
+        }
+
+        /// <summary>
+        /// Converts the specified nullable 64-bit signed integer to an enumeration member
+        /// </summary>
+        /// <typeparam name="T">The enumeration type to return</typeparam>
+        /// <param name="value">The value to convert to an enumeration member</param>
+        /// <param name="nullValue">The enum to be returned if the value is null</param>
+        /// <returns>The value converted to the enumeration; or nullValue if the value is null</returns>
+        /// <exception cref="ArgumentException">T is not an enum type</exception>
+        /// <exception cref="ArgumentOutOfRangeException">The value is not defined in the T enumeration</exception>
+        public static T ConvertEnum<T>(long? value, T nullValue)
+        {
+            return (!value.HasValue) ? nullValue : ConvertEnum<T>(value.Value);
+        }
+
+        /// <summary>
+        /// Converts the specified nullable 8-bit unsigned integer to an enumeration member
+        /// </summary>
+        /// <typeparam name="T">The enumeration type to return</typeparam>
+        /// <param name="value">The value to convert to an enumeration member</param>
+        /// <param name="nullValue">The enum to be returned if the value is null</param>
+        /// <returns>The value converted to the enumeration; or nullValue if the value is null</returns>
+        /// <exception cref="ArgumentException">T is not an enum type</exception>
+        /// <exception cref="ArgumentOutOfRangeException">The value is not defined in the T enumeration</exception>
+        public static T ConvertEnum<T>(ulong? value, T nullValue)
+        {
+            return (!value.HasValue) ? nullValue : ConvertEnum<T>(value.Value);
+        }
+
+        /// <summary>
+        /// Converts the specified number to an enumeration member
+        /// </summary>
+        /// <typeparam name="T">The enumeration type to return</typeparam>
+        /// <param name="value">The value to convert to an enumeration member</param>
+        /// <returns>The value converted to the enumeration</returns>
+        /// <exception cref="ArgumentException">T is not an enum type</exception>
+        /// <exception cref="ArgumentOutOfRangeException">The value is not defined in the T enumeration</exception>
+        public static T ConvertEnum<T>(decimal value)
+        {
+            switch (Type.GetTypeCode(Enum.GetUnderlyingType(typeof(T))))
+            {
+                case TypeCode.SByte:
+                    {
+                        var val = checked((sbyte)value);
+
+                        if (!Enum.IsDefined(typeof(T), val))
+                            throw new ArgumentOutOfRangeException(nameof(value));
+
+                        return (T)Enum.ToObject(typeof(T), val);
+                    }
+                case TypeCode.Int16:
+                    {
+                        var val = checked((short)value);
+
+                        if (!Enum.IsDefined(typeof(T), val))
+                            throw new ArgumentOutOfRangeException(nameof(value));
+
+                        return (T)Enum.ToObject(typeof(T), val);
+                    }
+                case TypeCode.Int32:
+                    {
+                        var val = checked((int)value);
+
+                        if (!Enum.IsDefined(typeof(T), val))
+                            throw new ArgumentOutOfRangeException(nameof(value));
+
+                        return (T)Enum.ToObject(typeof(T), val);
+                    }
+                case TypeCode.Int64:
+                    {
+                        var val = checked((long)value);
+
+                        if (!Enum.IsDefined(typeof(T), val))
+                            throw new ArgumentOutOfRangeException(nameof(value));
+
+                        return (T)Enum.ToObject(typeof(T), val);
+                    }
+                case TypeCode.Byte:
+                    {
+                        var val = checked((byte)value);
+
+                        if (!Enum.IsDefined(typeof(T), val))
+                            throw new ArgumentOutOfRangeException(nameof(value));
+
+                        return (T)Enum.ToObject(typeof(T), val);
+                    }
+                case TypeCode.UInt16:
+                    {
+                        var val = checked((ushort)value);
+
+                        if (!Enum.IsDefined(typeof(T), val))
+                            throw new ArgumentOutOfRangeException(nameof(value));
+
+                        return (T)Enum.ToObject(typeof(T), val);
+                    }
+                case TypeCode.UInt32:
+                    {
+                        var val = checked((uint)value);
+
+                        if (!Enum.IsDefined(typeof(T), val))
+                            throw new ArgumentOutOfRangeException(nameof(value));
+
+                        return (T)Enum.ToObject(typeof(T), val);
+                    }
+                case TypeCode.UInt64:
+                    {
+                        var val = checked((ulong)value);
+
+                        if (!Enum.IsDefined(typeof(T), val))
+                            throw new ArgumentOutOfRangeException(nameof(value));
+
+                        return (T)Enum.ToObject(typeof(T), val);
+                    }
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+    }
+}
---
+++ tests/WDNUtils.Common.Test/Common/EnumUtilsTest.cs
@@ -0,0 +1,160 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace WDNUtils.Common.Test
+{
+    [TestClass]
+    public class EnumUtilsTest
+    {
+        [TestMethod]
+        public void TestEnumUtils()
+        {
+            Assert.IsTrue(EnumUtils.GetValues<EnumSByte>().Select(item => (decimal)item).SequenceEqual(new decimal[] { 0, 1, 2, 4 }));
+
+            Assert.IsTrue((EnumSByte.V0 | EnumSByte.V1 | EnumSByte.V2).HasAnyFlag(EnumSByte.V0 | EnumSByte.V2));
+            Assert.IsFalse((EnumSByte.V0 | EnumSByte.V1 | EnumSByte.V2).HasAnyFlag(EnumSByte.V0 | EnumSByte.V4));
+            Assert.IsTrue(EnumSByte.V1.HasAnyBit(EnumSByte.V0 | EnumSByte.V1 | EnumSByte.V2));
+            Assert.IsFalse(EnumSByte.V4.HasAnyBit(EnumSByte.V0 | EnumSByte.V1 | EnumSByte.V2));
+            Assert.AreEqual(EnumSByte.V0, EnumUtils.ConvertEnum((sbyte?)null, EnumSByte.V0));
+            Assert.AreEqual(EnumSByte.V0, EnumUtils.ConvertEnum<EnumSByte>(0));
+            Assert.AreEqual(EnumSByte.V2, EnumUtils.ConvertEnum((sbyte?)2, EnumSByte.V0));
+            Assert.AreEqual(EnumSByte.V2, EnumUtils.ConvertEnum<EnumSByte>(2));
+
+            Assert.IsTrue((EnumByte.V0 | EnumByte.V1 | EnumByte.V2).HasAnyFlag(EnumByte.V0 | EnumByte.V2));
+            Assert.IsFalse((EnumByte.V0 | EnumByte.V1 | EnumByte.V2).HasAnyFlag(EnumByte.V0 | EnumByte.V4));
+            Assert.IsTrue(EnumByte.V1.HasAnyBit(EnumByte.V0 | EnumByte.V1 | EnumByte.V2));
+            Assert.IsFalse(EnumByte.V4.HasAnyBit(EnumByte.V0 | EnumByte.V1 | EnumByte.V2));
+            Assert.AreEqual(EnumByte.V0, EnumUtils.ConvertEnum((byte?)null, EnumByte.V0));
+            Assert.AreEqual(EnumByte.V0, EnumUtils.ConvertEnum<EnumByte>(0));
+            Assert.AreEqual(EnumByte.V2, EnumUtils.ConvertEnum((byte?)2, EnumByte.V0));
+            Assert.AreEqual(EnumByte.V2, EnumUtils.ConvertEnum<EnumByte>(2));
+
+            Assert.IsTrue((EnumUInt16.V0 | EnumUInt16.V1 | EnumUInt16.V2).HasAnyFlag(EnumUInt16.V0 | EnumUInt16.V2));
+            Assert.IsFalse((EnumUInt16.V0 | EnumUInt16.V1 | EnumUInt16.V2).HasAnyFlag(EnumUInt16.V0 | EnumUInt16.V4));
+            Assert.IsTrue(EnumUInt16.V1.HasAnyBit(EnumUInt16.V0 | EnumUInt16.V1 | EnumUInt16.V2));
+            Assert.IsFalse(EnumUInt16.V4.HasAnyBit(EnumUInt16.V0 | EnumUInt16.V1 | EnumUInt16.V2));
+            Assert.AreEqual(EnumUInt16.V0, EnumUtils.ConvertEnum((ushort?)null, EnumUInt16.V0));
+            Assert.AreEqual(EnumUInt16.V0, EnumUtils.ConvertEnum<EnumUInt16>(0));
+            Assert.AreEqual(EnumUInt16.V2, EnumUtils.ConvertEnum((ushort?)2, EnumUInt16.V0));
+            Assert.AreEqual(EnumUInt16.V2, EnumUtils.ConvertEnum<EnumUInt16>(2));
+
+            Assert.IsTrue((EnumInt16.V0 | EnumInt16.V1 | EnumInt16.V2).HasAnyFlag(EnumInt16.V0 | EnumInt16.V2));
+            Assert.IsFalse((EnumInt16.V0 | EnumInt16.V1 | EnumInt16.V2).HasAnyFlag(EnumInt16.V0 | EnumInt16.V4));
+            Assert.IsTrue(EnumInt16.V1.HasAnyBit(EnumInt16.V0 | EnumInt16.V1 | EnumInt16.V2));
+            Assert.IsFalse(EnumInt16.V4.HasAnyBit(EnumInt16.V0 | EnumInt16.V1 | EnumInt16.V2));
+            Assert.AreEqual(EnumInt16.V0, EnumUtils.ConvertEnum((short?)null, EnumInt16.V0));
+            Assert.AreEqual(EnumInt16.V0, EnumUtils.ConvertEnum<EnumInt16>(0));
+            Assert.AreEqual(EnumInt16.V2, EnumUtils.ConvertEnum((short?)2, EnumInt16.V0));
+            Assert.AreEqual(EnumInt16.V2, EnumUtils.ConvertEnum<EnumInt16>(2));
+
+            Assert.IsTrue((EnumUInt32.V0 | EnumUInt32.V1 | EnumUInt32.V2).HasAnyFlag(EnumUInt32.V0 | EnumUInt32.V2));
+            Assert.IsFalse((EnumUInt32.V0 | EnumUInt32.V1 | EnumUInt32.V2).HasAnyFlag(EnumUInt32.V0 | EnumUInt32.V4));
+            Assert.IsTrue(EnumUInt32.V1.HasAnyBit(EnumUInt32.V0 | EnumUInt32.V1 | EnumUInt32.V2));
+            Assert.IsFalse(EnumUInt32.V4.HasAnyBit(EnumUInt32.V0 | EnumUInt32.V1 | EnumUInt32.V2));
+            Assert.AreEqual(EnumUInt32.V0, EnumUtils.ConvertEnum((uint?)null, EnumUInt32.V0));
+            Assert.AreEqual(EnumUInt32.V0, EnumUtils.ConvertEnum<EnumUInt32>(0));
+            Assert.AreEqual(EnumUInt32.V2, EnumUtils.ConvertEnum((uint?)2, EnumUInt32.V0));
+            Assert.AreEqual(EnumUInt32.V2, EnumUtils.ConvertEnum<EnumUInt32>(2));
+
+            Assert.IsTrue((EnumInt32.V0 | EnumInt32.V1 | EnumInt32.V2).HasAnyFlag(EnumInt32.V0 | EnumInt32.V2));
+            Assert.IsFalse((EnumInt32.V0 | EnumInt32.V1 | EnumInt32.V2).HasAnyFlag(EnumInt32.V0 | EnumInt32.V4));
+            Assert.IsTrue(EnumInt32.V1.HasAnyBit(EnumInt32.V0 | EnumInt32.V1 | EnumInt32.V2));
+            Assert.IsFalse(EnumInt32.V4.HasAnyBit(EnumInt32.V0 | EnumInt32.V1 | EnumInt32.V2));
+            Assert.AreEqual(EnumInt32.V0, EnumUtils.ConvertEnum((int?)null, EnumInt32.V0));
+            Assert.AreEqual(EnumInt32.V0, EnumUtils.ConvertEnum<EnumInt32>(0));
+            Assert.AreEqual(EnumInt32.V2, EnumUtils.ConvertEnum((int?)2, EnumInt32.V0));
+            Assert.AreEqual(EnumInt32.V2, EnumUtils.ConvertEnum<EnumInt32>(2));
+
+            Assert.IsTrue((EnumUInt64.V0 | EnumUInt64.V1 | EnumUInt64.V2).HasAnyFlag(EnumUInt64.V0 | EnumUInt64.V2));
+            Assert.IsFalse((EnumUInt64.V0 | EnumUInt64.V1 | EnumUInt64.V2).HasAnyFlag(EnumUInt64.V0 | EnumUInt64.V4));
+            Assert.IsTrue(EnumUInt64.V1.HasAnyBit(EnumUInt64.V0 | EnumUInt64.V1 | EnumUInt64.V2));
+            Assert.IsFalse(EnumUInt64.V4.HasAnyBit(EnumUInt64.V0 | EnumUInt64.V1 | EnumUInt64.V2));
+            Assert.AreEqual(EnumUInt64.V0, EnumUtils.ConvertEnum((ulong?)null, EnumUInt64.V0));
+            Assert.AreEqual(EnumUInt64.V0, EnumUtils.ConvertEnum<EnumUInt64>(0));
+            Assert.AreEqual(EnumUInt64.V2, EnumUtils.ConvertEnum((ulong?)2, EnumUInt64.V0));
+            Assert.AreEqual(EnumUInt64.V2, EnumUtils.ConvertEnum<EnumUInt64>(2));
+
+            Assert.IsTrue((EnumInt64.V0 | EnumInt64.V1 | EnumInt64.V2).HasAnyFlag(EnumInt64.V0 | EnumInt64.V2));
+            Assert.IsFalse((EnumInt64.V0 | EnumInt64.V1 | EnumInt64.V2).HasAnyFlag(EnumInt64.V0 | EnumInt64.V4));
+            Assert.IsTrue(EnumInt64.V1.HasAnyBit(EnumInt64.V0 | EnumInt64.V1 | EnumInt64.V2));
+            Assert.IsFalse(EnumInt64.V4.HasAnyBit(EnumInt64.V0 | EnumInt64.V1 | EnumInt64.V2));
+            Assert.AreEqual(EnumInt64.V0, EnumUtils.ConvertEnum((long?)null, EnumInt64.V0));
+            Assert.AreEqual(EnumInt64.V0, EnumUtils.ConvertEnum<EnumInt64>(0));
+            Assert.AreEqual(EnumInt64.V2, EnumUtils.ConvertEnum((long?)2, EnumInt64.V0));
+            Assert.AreEqual(EnumInt64.V2, EnumUtils.ConvertEnum<EnumInt64>(2));
+        }
+
+        [Flags]
+        private enum EnumSByte : sbyte
+        {
+            V0 = 0,
+            V1 = 1,
+            V2 = 2,
+            V4 = 4
+        }
+
+        [Flags]
+        private enum EnumByte : byte
+        {
+            V0 = 0,
+            V1 = 1,
+            V2 = 2,
+            V4 = 4
+        }
+
+        [Flags]
+        private enum EnumInt16 : short
+        {
+            V0 = 0,
+            V1 = 1,
+            V2 = 2,
+            V4 = 4
+        }
+
+        [Flags]
+        private enum EnumUInt16 : ushort
+        {
+            V0 = 0,
+            V1 = 1,
+            V2 = 2,
+            V4 = 4
+        }
+
+        [Flags]
+        private enum EnumInt32 : int
+        {
+            V0 = 0,
+            V1 = 1,
+            V2 = 2,
+            V4 = 4
+        }
+
+        [Flags]
+        private enum EnumUInt32 : uint
+        {
+            V0 = 0,
+            V1 = 1,
+            V2 = 2,
+            V4 = 4
+        }
+
+        [Flags]
+        private enum EnumInt64 : long
+        {
+            V0 = 0,
+            V1 = 1,
+            V2 = 2,
+            V4 = 4
+        }
+
+        [Flags]
+        private enum EnumUInt64 : ulong
+        {
+            V0 = 0,
+            V1 = 1,
+            V2 = 2,
+            V4 = 4
+        }
+    }
+}
