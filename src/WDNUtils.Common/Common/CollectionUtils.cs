e1b688c43576a8095cd7a9c3b17da5ccdc5f9e81
rhrocha <rhrocha>
2018-10-02 11:45:12 -0300

    Added CollectionUtils class.

---
+++ src/WDNUtils.Common/Common/CollectionUtils.cs
@@ -0,0 +1,353 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WDNUtils.Common
+{
+    /// <summary>
+    /// Collection extensions
+    /// </summary>
+    public static class CollectionUtils
+    {
+        #region Dictionary extensions
+
+        /// <summary>
+        /// Gets the value associated with the specified key, or the default value if the dictionary does not contains an element with the specified key
+        /// </summary>
+        /// <typeparam name="TKey">The type of keys in the dictionary</typeparam>
+        /// <typeparam name="TValue">The type of values in the dictionary</typeparam>
+        /// <param name="dictionary">Collection of key/value pairs</param>
+        /// <param name="key">The key whose value to get</param>
+        /// <param name="defaultValue">Value to be returned if the dictionary does not contains an element with the specified key ('default(TValue)' if not specified)</param>
+        /// <returns>The value associated with the specified key, or the default value if the dictionary does not contains an element with the specified key</returns>
+        public static TValue GetOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue defaultValue = default(TValue))
+        {
+            return (dictionary.TryGetValue(key: key, value: out TValue value)) ? value : defaultValue;
+        }
+
+        /// <summary>
+        /// Gets the value associated with the specified key, or null if the dictionary does not contains an element with the specified key
+        /// </summary>
+        /// <typeparam name="TKey">The type of keys in the dictionary</typeparam>
+        /// <typeparam name="TValue">The type of values in the dictionary</typeparam>
+        /// <param name="dictionary">Collection of key/value pairs</param>
+        /// <param name="key">The key whose value to get</param>
+        /// <returns>The value associated with the specified key, or null if the dictionary does not contains an element with the specified key</returns>
+        public static TValue? GetOrNull<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key) where TValue : struct
+        {
+            return (dictionary.TryGetValue(key: key, value: out TValue value)) ? value : (TValue?)null;
+        }
+
+        #endregion
+
+        #region Nullable struct sequence extensions
+
+        /// <summary>
+        /// Converts a IEnumerable&lt;T&gt; into IEnumerable&lt;Nullable&lt;T&gt;&gt;, where T is a struct or primitive
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of source</typeparam>
+        /// <param name="list">The sequence to convert</param>
+        /// <returns>A sequence of Nullable&lt;T&gt;</returns>
+        public static IEnumerable<T?> AsNullable<T>(this IEnumerable<T> list) where T : struct
+        {
+            return list?.Select(selector: item => (T?)item);
+        }
+
+        /// <summary>
+        /// Returns the first element of a sequence, or null if the sequence contains no elements
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of source</typeparam>
+        /// <param name="list">The sequence to return the first element of</param>
+        /// <returns>The first element of the sequence, or null if the sequence contains no elements</returns>
+        public static T? FirstOrNull<T>(this IEnumerable<T> list) where T : struct
+        {
+            return list?.Select(selector: item => (T?)item)?.FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Returns the first element of the sequence that satisfies a condition or null if no such element is found
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of source</typeparam>
+        /// <param name="list">The sequence to return an element of</param>
+        /// <param name="predicate">A function to test each element for a condition</param>
+        /// <returns>The first element in source that passes the test specified by predicate, or null if source is empty or if no element passes the test specified by predicate</returns>
+        public static T? FirstOrNull<T>(this IEnumerable<T> list, Func<T, bool> predicate) where T : struct
+        {
+            return list?.Where(predicate: predicate)?.Select(selector: item => (T?)item)?.FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Returns the last element of a sequence, or null if the sequence contains no elements
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of source</typeparam>
+        /// <param name="list">The sequence to return the last element of</param>
+        /// <returns>The last element of the sequence, or null if the sequence contains no elements</returns>
+        public static T? LastOrNull<T>(this IEnumerable<T> list) where T : struct
+        {
+            return list?.Select(selector: item => (T?)item)?.LastOrDefault();
+        }
+
+        /// <summary>
+        /// Returns the last element of the sequence that satisfies a condition or null if no such element is found
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of source</typeparam>
+        /// <param name="list">The sequence to return an element of</param>
+        /// <param name="predicate">A function to test each element for a condition</param>
+        /// <returns>The last element in source that passes the test specified by predicate, or null if source is empty or if no element passes the test specified by predicate</returns>
+        public static T? LastOrNull<T>(this IEnumerable<T> list, Func<T, bool> predicate) where T : struct
+        {
+            return list?.Where(predicate: predicate)?.Select(selector: item => (T?)item)?.LastOrDefault();
+        }
+
+        /// <summary>
+        /// Returns the only element of a sequence, or a default value if the sequence is empty; this method throws an exception if there is more than one element in the sequence
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of source</typeparam>
+        /// <param name="list">The sequence to return the single element of</param>
+        /// <returns>The single element of the input sequence, or null if the sequence contains no elements</returns>
+        /// <exception cref="InvalidOperationException">The input sequence contains more than one element</exception>
+        public static T? SingleOrNull<T>(this IEnumerable<T> list) where T : struct
+        {
+            return list?.Select(selector: item => (T?)item)?.SingleOrDefault();
+        }
+
+        /// <summary>
+        /// Returns the only element of a sequence that satisfies a specified condition or a null if no such element exists; this method throws an exception if more than one element satisfies the condition
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of source</typeparam>
+        /// <param name="list">The sequence to return the single element of</param>
+        /// <param name="predicate">A function to test each element for a condition</param>
+        /// <returns>The single element of the input sequence that satisfies the condition, or null if no such element is found</returns>
+        /// <exception cref="InvalidOperationException">The input sequence contains more than one element that satisfies the condition</exception>
+        public static T? SingleOrNull<T>(this IEnumerable<T> list, Func<T, bool> predicate) where T : struct
+        {
+            return list?.Where(predicate: predicate)?.Select(selector: item => (T?)item)?.SingleOrDefault();
+        }
+
+        #endregion
+
+        #region List Shuffle
+
+        /// <summary>
+        /// Shuffles a list using Fisher–Yates algorithm, and returns itself (it does not create a new list)
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of source sequence</typeparam>
+        /// <param name="list">The list to shuffle</param>
+        /// <returns>A reference to the input list (it does not create a new list)</returns>
+        public static List<T> Shuffle<T>(this List<T> list)
+        {
+            for (int index = (list?.Count ?? 0) - 1; index > 0; index--)
+            {
+                int swap = RandomUtils.Next(maxValue: index + 1);
+
+                if (swap != index)
+                {
+                    T aux = list[index];
+                    list[index] = list[swap];
+                    list[swap] = aux;
+                }
+            }
+
+            return list;
+        }
+
+        /// <summary>
+        /// Take random elements from a sequence
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of source sequence</typeparam>
+        /// <param name="list">The sequence to take elements from</param>
+        /// <param name="count">The number of elements to take</param>
+        /// <returns>The random elements from the source sequence</returns>
+        public static IEnumerable<T> TakeShuffle<T>(this IEnumerable<T> list, int count)
+        {
+            T[] array = list.ToArray();
+
+            if (count > array.Length)
+            {
+                count = array.Length;
+            }
+
+            int max = array.Length;
+
+            while (count-- > 0)
+            {
+                int index = RandomUtils.Next(maxValue: max);
+
+                yield return array[index];
+
+                if (index < (max - 1))
+                {
+                    array[index] = array[max - 1];
+                }
+
+                max--;
+            }
+        }
+
+        #endregion
+
+        #region Split collection in limited length sublists
+
+        /// <summary>
+        /// Splits a list into sublists
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of source</typeparam>
+        /// <param name="list">The list to split</param>
+        /// <param name="sublistLength">The max length for the sublists</param>
+        /// <returns>A sequence of List&lt;T&gt;</returns>
+        public static IEnumerable<List<T>> SplitList<T>(this List<T> list, int sublistLength)
+        {
+            if (sublistLength < 1)
+                throw new ArgumentOutOfRangeException(paramName: nameof(sublistLength));
+
+            for (int remaining = list.Count, index = 0; remaining > 0; remaining -= sublistLength, index += sublistLength)
+            {
+                yield return list.GetRange(
+                    index: index,
+                    count: (remaining < sublistLength) ? remaining : sublistLength);
+            }
+        }
+
+        /// <summary>
+        /// Splits a sequence into sublists
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of source</typeparam>
+        /// <param name="collection">The sequence to split</param>
+        /// <param name="sublistLength">The max length for the sublists</param>
+        /// <returns>A sequence of List&lt;T&gt;</returns>
+        public static IEnumerable<List<T>> SplitList<T>(this IEnumerable<T> collection, int sublistLength)
+        {
+            return collection.ToList().SplitList(sublistLength: sublistLength);
+        }
+
+        #endregion
+
+        #region Remove duplicated items without changing sort order
+
+        /// <summary>
+        /// Removes duplicated elements of a list without changing its order
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of source</typeparam>
+        /// <param name="list">The source list to remove duplicated elements from</param>
+        /// <returns>A sequence with unique elements from the source list</returns>
+        public static IEnumerable<T> RemoveDuplicatesStable<T>(this List<T> list) where T : IEquatable<T>
+        {
+            var keyList = new HashSet<T>();
+
+            foreach (var item in list)
+            {
+                if (!keyList.Contains(item: item))
+                {
+                    keyList.Add(item: item);
+                    yield return item;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Removes duplicated elements of a list without changing its order
+        /// </summary>
+        /// <typeparam name="TValue">The type of the elements of source</typeparam>
+        /// <typeparam name="TKey">The type of the key used for equality comparison</typeparam>
+        /// <param name="list">The source list to remove duplicated elements from</param>
+        /// <param name="getKey">A function to get the key used for equality comparison for each element</param>
+        /// <returns>A sequence with unique elements from the source list</returns>
+        public static IEnumerable<TValue> RemoveDuplicatesStable<TValue, TKey>(this List<TValue> list, Func<TValue, TKey> getKey) where TKey : IEquatable<TKey>
+        {
+            var keyList = new HashSet<TKey>();
+
+            foreach (var item in list)
+            {
+                var key = getKey(arg: item);
+
+                if (!keyList.Contains(item: key))
+                {
+                    keyList.Add(item: key);
+                    yield return item;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Concatenate arrays
+
+        /// <summary>
+        /// Concatenate multiple arrays into a single array
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of source</typeparam>
+        /// <param name="sources">The source arrays to be concatenated</param>
+        /// <returns>An array with the concatenated elements of the source arrays</returns>
+        public static T[] ArrayConcat<T>(params T[][] sources)
+        {
+            int destinationIndex = 0;
+            int destinationSize = 0;
+
+            for (int index = 0; index < sources.Length; index++)
+            {
+                destinationSize += sources[index].Length;
+            }
+
+            var buffer = new T[destinationSize];
+
+            for (int index = 0; index < sources.Length; index++)
+            {
+                Array.Copy(
+                    sourceArray: sources[index],
+                    sourceIndex: 0,
+                    destinationArray: buffer,
+                    destinationIndex: destinationIndex,
+                    length: sources[index].Length);
+
+                destinationIndex += sources[index].Length;
+            }
+
+            return buffer;
+        }
+
+        #endregion
+
+        #region Split arrays
+
+        /// <summary>
+        /// Split an array in two subarrays, with a given max length for the first output array
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of source</typeparam>
+        /// <param name="input">The input array to be split</param>
+        /// <param name="output1Length">The max length of the first output subarray</param>
+        /// <param name="output1">The first output subarray, with the specified max length</param>
+        /// <param name="output2">The second output subarray, with the remaining elements</param>
+        public static void ArraySplit<T>(this T[] input, int output1Length, out T[] output1, out T[] output2)
+        {
+            if (output1Length < 0)
+                throw new ArgumentOutOfRangeException(paramName: nameof(output1Length));
+
+            if (input.Length > output1Length)
+            {
+                output1 = new T[output1Length];
+                output2 = new T[input.Length - output1Length];
+            }
+            else
+            {
+                output1 = new T[input.Length];
+                output2 = new T[0];
+            }
+
+            Array.Copy(
+                sourceArray: input,
+                sourceIndex: 0,
+                destinationArray: output1,
+                destinationIndex: 0,
+                length: output1.Length);
+
+            Array.Copy(
+                sourceArray: input,
+                sourceIndex: output1.Length,
+                destinationArray: output2,
+                destinationIndex: 0,
+                length: output2.Length);
+        }
+
+        #endregion
+    }
+}
---
+++ tests/WDNUtils.Common.Test/Common/CollectionUtilsTest.cs
@@ -0,0 +1,265 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WDNUtils.Common.Test
+{
+    [TestClass]
+    public class CollectionUtilsTest
+    {
+        [TestMethod]
+        public void TestCollectionUtilsDictionary()
+        {
+            var valueList = new Dictionary<int, int>() { { 0, 0 }, { 1, 1 }, { 3, 3 } };
+
+            Assert.AreEqual(0, valueList.GetOrDefault(0));
+            Assert.AreEqual(1, valueList.GetOrDefault(1));
+            Assert.AreEqual(0, valueList.GetOrDefault(2));
+            Assert.AreEqual(3, valueList.GetOrDefault(3));
+
+            Assert.AreEqual(0, valueList.GetOrDefault(0, -1));
+            Assert.AreEqual(1, valueList.GetOrDefault(1, -1));
+            Assert.AreEqual(-1, valueList.GetOrDefault(2, -1));
+            Assert.AreEqual(3, valueList.GetOrDefault(3, -1));
+
+            Assert.IsNotNull(valueList.GetOrNull(0));
+            Assert.IsNotNull(valueList.GetOrNull(1));
+            Assert.IsNull(valueList.GetOrNull(2));
+            Assert.IsNotNull(valueList.GetOrNull(3));
+
+            var textList = new Dictionary<string, string>() { { @"0", @"0" }, { @"1", @"1" }, { @"3", @"3" } };
+
+            Assert.AreEqual(@"0", textList.GetOrDefault(@"0"));
+            Assert.AreEqual(@"1", textList.GetOrDefault(@"1"));
+            Assert.IsNull(textList.GetOrDefault(@"2"));
+            Assert.AreEqual(@"3", textList.GetOrDefault(@"3"));
+
+            Assert.AreEqual(@"0", textList.GetOrDefault(@"0", @"-1"));
+            Assert.AreEqual(@"1", textList.GetOrDefault(@"1", @"-1"));
+            Assert.AreEqual(@"-1", textList.GetOrDefault(@"2", @"-1"));
+            Assert.AreEqual(@"3", textList.GetOrDefault(@"3", @"-1"));
+        }
+
+        [TestMethod]
+        public void TestCollectionUtilsNullable()
+        {
+            var emptyList = new List<int>();
+            var valueList = new List<int>() { 0, 1 };
+            var nullableList = new List<int?>() { 0, 1 };
+
+            Assert.IsTrue(Enumerable.SequenceEqual(valueList.AsNullable(), nullableList));
+
+            Assert.IsNull(emptyList.FirstOrNull());
+            Assert.IsNull(emptyList.FirstOrNull(item => true));
+            Assert.IsNull(emptyList.FirstOrNull(item => false));
+            Assert.IsNull(emptyList.LastOrNull());
+            Assert.IsNull(emptyList.LastOrNull(item => true));
+            Assert.IsNull(emptyList.LastOrNull(item => false));
+            Assert.IsNull(emptyList.SingleOrNull());
+            Assert.IsNull(emptyList.SingleOrNull(item => true));
+            Assert.IsNull(emptyList.SingleOrNull(item => false));
+
+            Assert.AreEqual(0, valueList.FirstOrNull());
+            Assert.AreEqual(0, valueList.FirstOrNull(item => item == 0));
+            Assert.AreEqual(1, valueList.FirstOrNull(item => item == 1));
+            Assert.AreEqual(null, valueList.FirstOrNull(item => item == 2));
+            Assert.AreEqual(1, valueList.LastOrNull());
+            Assert.AreEqual(0, valueList.LastOrNull(item => item == 0));
+            Assert.AreEqual(1, valueList.LastOrNull(item => item == 1));
+            Assert.AreEqual(null, valueList.LastOrNull(item => item == 2));
+
+            try
+            {
+                valueList.SingleOrNull();
+                Assert.Fail();
+            }
+            catch (InvalidOperationException)
+            {
+                // OK
+            }
+            Assert.AreEqual(0, valueList.SingleOrNull(item => item == 0));
+            Assert.AreEqual(1, valueList.SingleOrNull(item => item == 1));
+            Assert.AreEqual(null, valueList.SingleOrNull(item => item == 2));
+        }
+
+        [TestMethod]
+        public void TestCollectionUtilsShuffle()
+        {
+            var valueList = new List<int>() { 0, 1, 2, 4, 8, 16, 32 };
+
+            var test = valueList.ToList().Shuffle();
+
+            while (Enumerable.SequenceEqual(test, valueList))
+            {
+                test = test.Shuffle();
+            }
+
+            Assert.IsTrue(Enumerable.SequenceEqual(valueList.OrderBy(item => item), test.OrderBy(item => item)));
+
+            test = valueList.TakeShuffle(5).ToList();
+
+            Assert.AreEqual(5, test.Count);
+            Assert.IsTrue(Enumerable.SequenceEqual(test.OrderBy(item => item), test.Intersect(valueList).OrderBy(item => item)));
+        }
+
+        [TestMethod]
+        public void TestCollectionUtilsSplitList()
+        {
+            for (int repeat = 0; repeat < 1000; repeat++)
+            {
+                int listSize = (repeat > 5) ? RandomUtils.NextInt32(0, 100) : repeat;
+                int sublistLength = RandomUtils.NextInt32(1, 100);
+
+                var valueList = RandomUtils.NextBytes(listSize);
+
+                var sublists = valueList.SplitList(sublistLength).ToList();
+
+                Assert.AreEqual(Convert.ToInt32(Math.Ceiling((float)listSize / sublistLength)), sublists.Count);
+
+                int offset = 0;
+                var lastSublistSize = listSize % sublistLength;
+
+                if (lastSublistSize == 0)
+                {
+                    lastSublistSize = sublistLength;
+                }
+
+                for (int index = 0; index < sublists.Count; index++)
+                {
+                    Assert.AreEqual(
+                        (index < (sublists.Count - 1)) ? sublistLength : lastSublistSize,
+                        sublists[index].Count);
+
+                    Assert.IsTrue(Enumerable.SequenceEqual(valueList.Skip(offset).Take(sublistLength), sublists[index]));
+
+                    offset += sublists[index].Count;
+                }
+            }
+        }
+
+        private class TestItem : IEquatable<TestItem>
+        {
+            public int Key { get; set; }
+            public int Value { get; set; }
+
+            bool IEquatable<TestItem>.Equals(TestItem other)
+            {
+                return Value == other?.Value;
+            }
+
+            public override int GetHashCode()
+            {
+                return Value.GetHashCode();
+            }
+        }
+
+        [TestMethod]
+        public void TestCollectionUtilsDuplicates()
+        {
+            var testList = new List<TestItem>()
+            {
+                new TestItem() { Key = 0, Value = 1 },
+                new TestItem() { Key = 2, Value = 1 },
+                new TestItem() { Key = 0, Value = 2 },
+                new TestItem() { Key = 1, Value = 0 },
+                new TestItem() { Key = 2, Value = 0 },
+                new TestItem() { Key = 1, Value = 2 },
+            };
+
+            var firstKeyList = new List<TestItem>()
+            {
+                new TestItem() { Key = 0, Value = 1 },
+                new TestItem() { Key = 2, Value = 1 },
+                new TestItem() { Key = 1, Value = 0 },
+            };
+
+            var firstValueList = new List<TestItem>()
+            {
+                new TestItem() { Key = 0, Value = 1 },
+                new TestItem() { Key = 0, Value = 2 },
+                new TestItem() { Key = 1, Value = 0 },
+            };
+
+            Assert.IsTrue(Enumerable.SequenceEqual(firstKeyList, testList.RemoveDuplicatesStable(item => item.Key)));
+            Assert.IsTrue(Enumerable.SequenceEqual(firstValueList, testList.RemoveDuplicatesStable()));
+
+            testList = testList.OrderBy(item => item.Key).ThenBy(item => item.Value).ToList();
+
+            firstKeyList = new List<TestItem>()
+            {
+                new TestItem() { Key = 0, Value = 1 },
+                new TestItem() { Key = 1, Value = 0 },
+                new TestItem() { Key = 2, Value = 0 },
+            };
+
+            firstValueList = new List<TestItem>()
+            {
+                new TestItem() { Key = 0, Value = 1 },
+                new TestItem() { Key = 0, Value = 2 },
+                new TestItem() { Key = 1, Value = 0 },
+            };
+
+            Assert.IsTrue(Enumerable.SequenceEqual(firstKeyList, testList.RemoveDuplicatesStable(item => item.Key)));
+            Assert.IsTrue(Enumerable.SequenceEqual(firstValueList, testList.RemoveDuplicatesStable()));
+
+            testList = testList.OrderBy(item => item.Value).ThenBy(item => item.Key).ToList();
+
+            firstKeyList = new List<TestItem>()
+            {
+                new TestItem() { Key = 1, Value = 0 },
+                new TestItem() { Key = 2, Value = 0 },
+                new TestItem() { Key = 0, Value = 1 },
+            };
+
+            firstValueList = new List<TestItem>()
+            {
+                new TestItem() { Key = 1, Value = 0 },
+                new TestItem() { Key = 0, Value = 1 },
+                new TestItem() { Key = 0, Value = 2 },
+            };
+
+            Assert.IsTrue(Enumerable.SequenceEqual(firstKeyList, testList.RemoveDuplicatesStable(item => item.Key)));
+            Assert.IsTrue(Enumerable.SequenceEqual(firstValueList, testList.RemoveDuplicatesStable()));
+        }
+
+        [TestMethod]
+        public void TestCollectionUtilsConcatSplit()
+        {
+            for (int index = 0; index < 10; index++)
+            {
+                var input = RandomUtils.NextBytes(0, 10);
+
+                var size1 = RandomUtils.NextInt32(0, input.Length);
+                var size2 = RandomUtils.NextInt32(size1, input.Length) - size1;
+
+                input.ArraySplit(size1, out var output1, out var outputTemp);
+                outputTemp.ArraySplit(size2, out var output2, out var output3);
+
+                Assert.AreEqual(size1, output1.Length);
+                Assert.AreEqual(size2, output2.Length);
+                Assert.AreEqual(input.Length - size1 - size2, output3.Length);
+
+                var output = output1.Concat(output2).Concat(output3).ToArray();
+
+                Assert.AreEqual(input.Length, output.Length);
+                Assert.IsTrue(Enumerable.SequenceEqual(input, output));
+
+                var concat1 = CollectionUtils.ArrayConcat(output1);
+
+                Assert.AreEqual(output1.Length, concat1.Length);
+                Assert.IsTrue(Enumerable.SequenceEqual(output1, concat1));
+
+                var concatTemp = CollectionUtils.ArrayConcat(output2, output3);
+
+                Assert.AreEqual(outputTemp.Length, concatTemp.Length);
+                Assert.IsTrue(Enumerable.SequenceEqual(outputTemp, concatTemp));
+
+                var concat = CollectionUtils.ArrayConcat(output1, output2, output3);
+
+                Assert.AreEqual(input.Length, concat.Length);
+                Assert.IsTrue(Enumerable.SequenceEqual(input, concat));
+            }
+        }
+    }
+}
