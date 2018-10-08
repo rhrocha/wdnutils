eeb01a149c66c1638afa9c3b65060cb6f12adc0f
rhrocha <rhrocha>
2018-10-08 14:56:02 -0300

    Added ODP.NET wrapper projects for Core and net461.

--- src/cleanup.bat
+++ src/cleanup.bat
@@ -11,6 +11,24 @@ IF EXIST ".\WDNUtils.Common\obj" DEL /A /F /S /Q ".\WDNUtils.Common\obj"
 IF EXIST ".\WDNUtils.Common\obj" RMDIR /S /Q ".\WDNUtils.Common\obj"
 
 REM ========================================================================
+ECHO ### Clean up WDNUtils.Oracle.Core
+
+IF EXIST ".\WDNUtils.Oracle.Core\WDNUtils.Oracle.Core.csproj.user" DEL /A /F /Q ".\WDNUtils.Oracle.Core\WDNUtils.Oracle.Core.csproj.user"
+IF EXIST ".\WDNUtils.Oracle.Core\bin" DEL /A /F /S /Q ".\WDNUtils.Oracle.Core\bin"
+IF EXIST ".\WDNUtils.Oracle.Core\bin" RMDIR /S /Q ".\WDNUtils.Oracle.Core\bin"
+IF EXIST ".\WDNUtils.Oracle.Core\obj" DEL /A /F /S /Q ".\WDNUtils.Oracle.Core\obj"
+IF EXIST ".\WDNUtils.Oracle.Core\obj" RMDIR /S /Q ".\WDNUtils.Oracle.Core\obj"
+
+REM ========================================================================
+ECHO ### Clean up WDNUtils.Oracle.Win32
+
+IF EXIST ".\WDNUtils.Oracle.Win32\WDNUtils.Oracle.Win32.csproj.user" DEL /A /F /Q ".\WDNUtils.Oracle.Win32\WDNUtils.Oracle.Win32.csproj.user"
+IF EXIST ".\WDNUtils.Oracle.Win32\bin" DEL /A /F /S /Q ".\WDNUtils.Oracle.Win32\bin"
+IF EXIST ".\WDNUtils.Oracle.Win32\bin" RMDIR /S /Q ".\WDNUtils.Oracle.Win32\bin"
+IF EXIST ".\WDNUtils.Oracle.Win32\obj" DEL /A /F /S /Q ".\WDNUtils.Oracle.Win32\obj"
+IF EXIST ".\WDNUtils.Oracle.Win32\obj" RMDIR /S /Q ".\WDNUtils.Oracle.Win32\obj"
+
+REM ========================================================================
 ECHO ### Clean up vistual studio generated files
 
 IF EXIST ".\.vs\" DEL /A /F /S /Q ".\.vs"
--- tests/cleanup.bat
+++ tests/cleanup.bat
@@ -11,6 +11,24 @@ IF EXIST "..\src\WDNUtils.Common\obj" DEL /A /F /S /Q "..\src\WDNUtils.Common\ob
 IF EXIST "..\src\WDNUtils.Common\obj" RMDIR /S /Q "..\src\WDNUtils.Common\obj"
 
 REM ========================================================================
+ECHO ### Clean up WDNUtils.Oracle.Core
+
+IF EXIST "..\src\WDNUtils.Oracle.Core\WDNUtils.Oracle.Core.csproj.user" DEL /A /F /Q "..\src\WDNUtils.Oracle.Core\WDNUtils.Oracle.Core.csproj.user"
+IF EXIST "..\src\WDNUtils.Oracle.Core\bin" DEL /A /F /S /Q "..\src\WDNUtils.Oracle.Core\bin"
+IF EXIST "..\src\WDNUtils.Oracle.Core\bin" RMDIR /S /Q "..\src\WDNUtils.Oracle.Core\bin"
+IF EXIST "..\src\WDNUtils.Oracle.Core\obj" DEL /A /F /S /Q "..\src\WDNUtils.Oracle.Core\obj"
+IF EXIST "..\src\WDNUtils.Oracle.Core\obj" RMDIR /S /Q "..\src\WDNUtils.Oracle.Core\obj"
+
+REM ========================================================================
+ECHO ### Clean up WDNUtils.Oracle.Win32
+
+IF EXIST "..\src\WDNUtils.Oracle.Win32\WDNUtils.Oracle.Win32.csproj.user" DEL /A /F /Q "..\src\WDNUtils.Oracle.Win32\WDNUtils.Oracle.Win32.csproj.user"
+IF EXIST "..\src\WDNUtils.Oracle.Win32\bin" DEL /A /F /S /Q "..\src\WDNUtils.Oracle.Win32\bin"
+IF EXIST "..\src\WDNUtils.Oracle.Win32\bin" RMDIR /S /Q "..\src\WDNUtils.Oracle.Win32\bin"
+IF EXIST "..\src\WDNUtils.Oracle.Win32\obj" DEL /A /F /S /Q "..\src\WDNUtils.Oracle.Win32\obj"
+IF EXIST "..\src\WDNUtils.Oracle.Win32\obj" RMDIR /S /Q "..\src\WDNUtils.Oracle.Win32\obj"
+
+REM ========================================================================
 ECHO ### Clean up WDNUtils.Common.Test
 
 IF EXIST ".\WDNUtils.Common.Test\WDNUtils.Common.Test.csproj.user" DEL /A /F /Q ".\WDNUtils.Common.Test\WDNUtils.Common.Test.csproj.user"
