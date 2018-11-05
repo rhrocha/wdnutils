7c96ede9efd80a65808bd29f22446e5d914d86c1
rhrocha <rhrocha>
2018-11-05 14:43:45 -0200

    Added connection string builder for SQL Server.

---
+++ src/WDNUtils.DBSqlServer/Classes/DBSqlServerConnectionStringBuilder.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace WDNUtils.DBSqlServer
+{
+    /// <summary>
+    /// Wrapper class for SqlConnectionStringBuilder
+    /// </summary>
+    public sealed class DBSqlServerConnectionStringBuilder
+    {
+        #region Attributes
+
+        private readonly SqlConnectionStringBuilder _builder = new SqlConnectionStringBuilder();
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Returns the connection string with the settings of this instance
+        /// </summary>
+        public string ConnectionString => _builder.ConnectionString;
+
+        /// <summary>
+        /// Gets or sets the user ID to be used when connecting to SQL Server
+        /// </summary>
+        public string UserID { get => _builder.UserID; set { if (value == null) { RemoveProperties(@"User ID", @"user", @"uid"); } else { _builder.UserID = value; } } }
+
+        /// <summary>
+        /// Gets or sets a string value that indicates the type system the application expects
+        /// </summary>
+        public string TypeSystemVersion { get => _builder.TypeSystemVersion; set => _builder.TypeSystemVersion = value; }
+
+        /// <summary>
+        /// Gets or sets a value that indicates whether the channel will be encrypted while bypassing walking the certificate chain to validate trust
+        /// </summary>
+        public bool TrustServerCertificate { get => _builder.TrustServerCertificate; set => _builder.TrustServerCertificate = value; }
+
+        /// <summary>
+        /// Gets or sets a string value that indicates how the connection maintains its association with an enlisted System.Transactions transaction
+        /// </summary>
+        public bool? TransactionBindingExplicit
+        {
+            get => string.Equals(_builder.TransactionBinding, @"Explicit Unbind", StringComparison.OrdinalIgnoreCase) ? true :
+                string.Equals(_builder.TransactionBinding, @"Implicit Unbind", StringComparison.OrdinalIgnoreCase) ? (bool?)false :
+                null;
+            set { if (value == null) { RemoveProperties(@"Transaction Binding"); } else { _builder.TransactionBinding = value.Value ? @"Explicit Unbind" : @"Implicit Unbind"; } }
+        }
+
+        /// <summary>
+        /// Gets or sets a Boolean value that indicates whether replication is supported using the connection
+        /// </summary>
+        public bool Replication { get => _builder.Replication; set => _builder.Replication = value; }
+
+        /// <summary>
+        /// Gets or sets a Boolean value that indicates whether the connection will be pooled or explicitly opened every time that the connection is requested
+        /// </summary>
+        public bool Pooling { get => _builder.Pooling; set => _builder.Pooling = value; }
+
+        /// <summary>
+        /// Gets or sets the password for the SQL Server account
+        /// </summary>
+        public string Password { get => _builder.Password; set => _builder.Password = value; }
+
+        /// <summary>
+        /// Gets or sets the size in bytes of the network packets used to communicate with an instance of SQL Server
+        /// </summary>
+        public int PacketSize { get => _builder.PacketSize; set => _builder.PacketSize = value; }
+
+        /// <summary>
+        /// Gets or sets a Boolean value that enables faster detection of and connection to the active server is enabled for AlwaysOn availability group on different subnets
+        /// </summary>
+        public bool MultiSubnetFailover { get => _builder.MultiSubnetFailover; set => _builder.MultiSubnetFailover = value; }
+
+        /// <summary>
+        /// Gets or sets a Boolean value that enables multiple active result sets (MARS)
+        public bool MultipleActiveResultSets { get => _builder.MultipleActiveResultSets; set => _builder.MultipleActiveResultSets = value; }
+
+        /// <summary>
+        /// Gets or sets the minimum number of connections allowed in the connection pool for this specific connection string
+        /// </summary>
+        public int MinPoolSize { get => _builder.MinPoolSize; set => _builder.MinPoolSize = value; }
+
+        /// <summary>
+        /// Gets or sets the maximum number of connections allowed in the connection pool for this specific connection string
+        /// </summary>
+        public int MaxPoolSize { get => _builder.MaxPoolSize; set => _builder.MaxPoolSize = value; }
+
+        /// <summary>
+        /// Gets or sets the minimum time, in seconds, for the connection to live in the connection pool before being destroyed
+        /// </summary>
+        public int LoadBalanceTimeout { get => _builder.LoadBalanceTimeout; set => _builder.LoadBalanceTimeout = value; }
+
+        /// <summary>
+        /// Gets or sets a value that indicates whether to redirect the connection from the default SQL Server Express instance to a runtime-initiated instance running under the account of the caller
+        /// </summary>
+        public bool? UserInstance { get => _builder.UserInstance; set { if (value == null) { RemoveProperties(@"User Instance"); } else { _builder.UserInstance = value.Value; } } }
+
+        /// <summary>
+        /// Gets or sets a Boolean value that indicates if security-sensitive information, such as the password, is not returned as part of the connection if the connection is open or has ever been in an open state
+        /// </summary>
+        public bool PersistSecurityInfo { get => _builder.PersistSecurityInfo; set => _builder.PersistSecurityInfo = value; }
+
+        /// <summary>
+        /// Gets or sets a Boolean value that indicates whether User ID and Password are specified in the connection (when false) or whether the current Windows account credentials are used for authentication (when true)
+        /// </summary>
+        public bool IntegratedSecurity { get => _builder.IntegratedSecurity; set => _builder.IntegratedSecurity = value; }
+
+        /// <summary>
+        /// Gets or sets the name of the database associated with the connection
+        /// </summary>
+        public string InitialCatalog { get => _builder.InitialCatalog; set { if (value == null) { RemoveProperties(@"Initial Catalog", @"database"); } else { _builder.InitialCatalog = value; } } }
+
+        /// <summary>
+        /// Gets or sets a Boolean value that indicates whether the application workload type will be read-only or read/write (default is false, for read/write workload)
+        /// </summary>
+        public bool ReadOnly { get => _builder.ApplicationIntent == ApplicationIntent.ReadOnly; set => _builder.ApplicationIntent = value ? ApplicationIntent.ReadOnly : ApplicationIntent.ReadWrite; }
+
+        /// <summary>
+        /// Gets or sets the name of the application associated with the connection string
+        /// </summary>
+        public string ApplicationName { get => _builder.ApplicationName; set { if (value == null) { RemoveProperties(@"Application Name", @"app"); } else { _builder.ApplicationName = value; } } }
+
+        /// <summary>
+        /// Gets or sets the number of reconnections attempted after identifying that there was an idle connection failure
+        /// </summary>
+        public int ConnectRetryCount { get => _builder.ConnectRetryCount; set => _builder.ConnectRetryCount = value; }
+
+        /// <summary>
+        /// Gets or sets the amount of time (in seconds) between each reconnection attempt after identifying that there was an idle connection failure
+        /// </summary>
+        public int ConnectRetryInterval { get => _builder.ConnectRetryInterval; set => _builder.ConnectRetryInterval = value; }
+
+        /// <summary>
+        /// Gets or sets a string that contains the name of the primary data file (includes the full path name of an attachable database)
+        /// </summary>
+        public string AttachDBFilename { get => _builder.AttachDBFilename; set { if (value == null) { RemoveProperties(@"AttachDBFilename", @"extended properties", @"initial file name"); } else { _builder.AttachDBFilename = value; } } }
+
+        /// <summary>
+        /// Gets or sets the SQL Server Language record name
+        /// </summary>
+        public string CurrentLanguage { get => _builder.CurrentLanguage; set { if (value == null) { RemoveProperties(@"Current Language", @"language"); } else { _builder.CurrentLanguage = value; } } }
+
+        /// <summary>
+        /// Gets or sets the name or network address of the instance of SQL Server to connect to
+        /// </summary>
+        public string DataSource { get => _builder.DataSource; set { if (value == null) { RemoveProperties(@"Data Source", @"server", @"address", @"addr", @"network address"); } else { _builder.DataSource = value; } } }
+
+        /// <summary>
+        /// Gets or sets a Boolean value that indicates whether SQL Server uses SSL encryption for all data sent between the client and server if the server has a certificate installed
+        /// </summary>
+        public bool Encrypt { get => _builder.Encrypt; set => _builder.Encrypt = value; }
+
+        /// <summary>
+        /// Gets or sets a Boolean value that indicates whether the SQL Server connection pooler automatically enlists the connection in the creation thread's current transaction context
+        /// </summary>
+        public bool Enlist { get => _builder.Enlist; set => _builder.Enlist = value; }
+
+        /// <summary>
+        /// Gets or sets the name or address of the partner server to connect to if the primary server is down
+        /// </summary>
+        public string FailoverPartner { get => _builder.FailoverPartner; set { if (value == null) { RemoveProperties(@"Failover Partner"); } else { _builder.FailoverPartner = value; } } }
+
+        /// <summary>
+        /// Gets or sets the length of time (in seconds) to wait for a connection to the server before terminating the attempt and generating an error
+        /// </summary>
+        public int ConnectTimeout { get => _builder.ConnectTimeout; set => _builder.ConnectTimeout = value; }
+
+        /// <summary>
+        /// Gets or sets the name of the workstation connecting to SQL Server
+        /// </summary>
+        public string WorkstationID { get => _builder.WorkstationID; set { if (value == null) { RemoveProperties(@"Workstation ID", @"wsid"); } else { _builder.WorkstationID = value; } } }
+
+        #endregion
+
+        #region Remove properties from string builder
+
+        /// <summary>
+        /// Remove properties from string builder
+        /// </summary>
+        /// <param name="properties">Properties to be removed from string builder</param>
+        private void RemoveProperties(params string[] properties)
+        {
+            if (!(properties?.Length > 0))
+                return;
+
+            var propertyList = new HashSet<string>(properties.Select(item => item.ToUpperInvariant()));
+
+            var keyList = _builder.Keys.OfType<string>().Where(key => propertyList.Contains(key.ToUpperInvariant()));
+
+            foreach (var key in keyList)
+            {
+                _builder.Remove(key);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ tests/WDNUtils.DBSqlServer.Test/Classes/DBSqlServerConnectionStringBuilderTest.cs
@@ -0,0 +1,202 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace WDNUtils.DBSqlServer.Test
+{
+    [TestClass]
+    public class DBSqlServerConnectionStringBuilderTest
+    {
+        [TestMethod]
+        public void TestDBSqlServerConnectionStringBuilderProperties()
+        {
+            var builder = new DBSqlServerConnectionStringBuilder() { ApplicationName = @"WDNUtils Unit Test" };
+            Assert.AreEqual(@"Application Name=""WDNUtils Unit Test""", builder.ConnectionString);
+            builder.ApplicationName = null;
+            Assert.AreEqual(string.Empty, builder.ConnectionString);
+
+            builder = new DBSqlServerConnectionStringBuilder() { AttachDBFilename = @"test.db" };
+            Assert.AreEqual(@"AttachDbFilename=test.db", builder.ConnectionString);
+            builder.AttachDBFilename = null;
+            Assert.AreEqual(string.Empty, builder.ConnectionString);
+
+            builder = new DBSqlServerConnectionStringBuilder() { ConnectRetryCount = 1 };
+            Assert.AreEqual(@"ConnectRetryCount=1", builder.ConnectionString);
+
+            builder = new DBSqlServerConnectionStringBuilder() { ConnectRetryInterval = 2 };
+            Assert.AreEqual(@"ConnectRetryInterval=2", builder.ConnectionString);
+
+            builder = new DBSqlServerConnectionStringBuilder() { ConnectTimeout = 3 };
+            Assert.AreEqual(@"Connect Timeout=3", builder.ConnectionString);
+
+            builder = new DBSqlServerConnectionStringBuilder() { CurrentLanguage = @"en-US" };
+            Assert.AreEqual(@"Current Language=en-US", builder.ConnectionString);
+            builder.CurrentLanguage = null;
+            Assert.AreEqual(string.Empty, builder.ConnectionString);
+
+            builder = new DBSqlServerConnectionStringBuilder() { DataSource = @"192.167.0.1" };
+            Assert.AreEqual(@"Data Source=192.167.0.1", builder.ConnectionString);
+            builder.DataSource = null;
+            Assert.AreEqual(string.Empty, builder.ConnectionString);
+
+            builder = new DBSqlServerConnectionStringBuilder() { Encrypt = true };
+            Assert.AreEqual(@"Encrypt=True", builder.ConnectionString);
+
+            builder = new DBSqlServerConnectionStringBuilder() { Enlist = true };
+            Assert.AreEqual(@"Enlist=True", builder.ConnectionString);
+
+            builder = new DBSqlServerConnectionStringBuilder() { FailoverPartner = @"192.167.0.2" };
+            Assert.AreEqual(@"Failover Partner=192.167.0.2", builder.ConnectionString);
+            builder.FailoverPartner = null;
+            Assert.AreEqual(string.Empty, builder.ConnectionString);
+
+            builder = new DBSqlServerConnectionStringBuilder() { InitialCatalog = @"<database_name>" };
+            Assert.AreEqual(@"Initial Catalog=<database_name>", builder.ConnectionString);
+            builder.InitialCatalog = null;
+            Assert.AreEqual(string.Empty, builder.ConnectionString);
+
+            builder = new DBSqlServerConnectionStringBuilder() { IntegratedSecurity = true };
+            Assert.AreEqual(@"Integrated Security=True", builder.ConnectionString);
+
+            builder = new DBSqlServerConnectionStringBuilder() { LoadBalanceTimeout = 4 };
+            Assert.AreEqual(@"Load Balance Timeout=4", builder.ConnectionString);
+
+            builder = new DBSqlServerConnectionStringBuilder() { MaxPoolSize = 6 };
+            Assert.AreEqual(@"Max Pool Size=6", builder.ConnectionString);
+
+            builder = new DBSqlServerConnectionStringBuilder() { MinPoolSize = 5 };
+            Assert.AreEqual(@"Min Pool Size=5", builder.ConnectionString);
+
+            builder = new DBSqlServerConnectionStringBuilder() { MultipleActiveResultSets = true };
+            Assert.AreEqual(@"MultipleActiveResultSets=True", builder.ConnectionString);
+
+            builder = new DBSqlServerConnectionStringBuilder() { MultiSubnetFailover = true };
+            Assert.AreEqual(@"MultiSubnetFailover=True", builder.ConnectionString);
+
+            builder = new DBSqlServerConnectionStringBuilder() { PacketSize = 2048 };
+            Assert.AreEqual(@"Packet Size=2048", builder.ConnectionString);
+
+            builder = new DBSqlServerConnectionStringBuilder() { Password = @"test" };
+            Assert.AreEqual(@"Password=test", builder.ConnectionString);
+
+            builder = new DBSqlServerConnectionStringBuilder() { Password = @"test'" };
+            Assert.AreEqual(@"Password=""test'""", builder.ConnectionString);
+
+            builder = new DBSqlServerConnectionStringBuilder() { Password = @"test""" };
+            Assert.AreEqual(@"Password='test""'", builder.ConnectionString);
+
+            builder = new DBSqlServerConnectionStringBuilder() { Password = @"test""'" };
+            Assert.AreEqual(@"Password=""test""""'""", builder.ConnectionString);
+
+            builder = new DBSqlServerConnectionStringBuilder() { PersistSecurityInfo = true };
+            Assert.AreEqual(@"Persist Security Info=True", builder.ConnectionString);
+
+            builder = new DBSqlServerConnectionStringBuilder() { Pooling = true };
+            Assert.AreEqual(@"Pooling=True", builder.ConnectionString);
+
+            builder = new DBSqlServerConnectionStringBuilder() { ReadOnly = true };
+            Assert.AreEqual(@"ApplicationIntent=ReadOnly", builder.ConnectionString);
+
+            builder = new DBSqlServerConnectionStringBuilder() { Replication = true };
+            Assert.AreEqual(@"Replication=True", builder.ConnectionString);
+
+            builder = new DBSqlServerConnectionStringBuilder() { TransactionBindingExplicit = true };
+            Assert.AreEqual(@"Transaction Binding=""Explicit Unbind""", builder.ConnectionString);
+            builder.TransactionBindingExplicit = null;
+            Assert.AreEqual(string.Empty, builder.ConnectionString);
+
+            builder = new DBSqlServerConnectionStringBuilder() { TrustServerCertificate = true };
+            Assert.AreEqual(@"TrustServerCertificate=True", builder.ConnectionString);
+
+            builder = new DBSqlServerConnectionStringBuilder() { TypeSystemVersion = @"SQL Server 2012" };
+            Assert.AreEqual(@"Type System Version=""SQL Server 2012""", builder.ConnectionString);
+
+            builder = new DBSqlServerConnectionStringBuilder() { UserID = @"<user_id>" };
+            Assert.AreEqual(@"User ID=<user_id>", builder.ConnectionString);
+            builder.UserID = null;
+            Assert.AreEqual(string.Empty, builder.ConnectionString);
+
+            builder = new DBSqlServerConnectionStringBuilder() { UserInstance = true };
+            Assert.AreEqual(@"User Instance=True", builder.ConnectionString);
+            builder.UserInstance = null;
+            Assert.AreEqual(string.Empty, builder.ConnectionString);
+
+            builder = new DBSqlServerConnectionStringBuilder() { WorkstationID = @"localhost" };
+            Assert.AreEqual(@"Workstation ID=localhost", builder.ConnectionString);
+            builder.WorkstationID = null;
+            Assert.AreEqual(string.Empty, builder.ConnectionString);
+        }
+
+        [TestMethod]
+        public void TestDBSqlServerConnectionStringBuilderAll()
+        {
+            var builder = new DBSqlServerConnectionStringBuilder()
+            {
+                ApplicationName = @"WDNUtils Unit Test",
+                ReadOnly = true,
+                AttachDBFilename = @"test.db",
+                ConnectTimeout = 3,
+                ConnectRetryCount = 1,
+                ConnectRetryInterval = 2,
+                CurrentLanguage = @"en-US",
+                DataSource = @"192.167.0.1",
+                Encrypt = true,
+                Enlist = true,
+                FailoverPartner = @"192.167.0.2",
+                InitialCatalog = @"<database_name>",
+                IntegratedSecurity = true,
+                LoadBalanceTimeout = 4,
+                MaxPoolSize = 6,
+                MinPoolSize = 5,
+                MultipleActiveResultSets = true,
+                MultiSubnetFailover = true,
+                PacketSize = 2048,
+                Password = @"test""'",
+                PersistSecurityInfo = true,
+                Pooling = true,
+                Replication = true,
+                TransactionBindingExplicit = true,
+                TrustServerCertificate = true,
+                TypeSystemVersion = @"SQL Server 2012",
+                UserID = @"<user_id>",
+                UserInstance = true,
+                WorkstationID = @"localhost"
+            };
+
+            var values = builder.ConnectionString.Split(';').OrderBy(item => item).ToList();
+
+            var expected = new[] {
+                @"Application Name=""WDNUtils Unit Test""",
+                @"ApplicationIntent=ReadOnly",
+                @"AttachDbFilename=test.db",
+                @"Connect Timeout=3",
+                @"ConnectRetryCount=1",
+                @"ConnectRetryInterval=2",
+                @"Current Language=en-US",
+                @"Data Source=192.167.0.1",
+                @"Encrypt=True",
+                @"Enlist=True",
+                @"Failover Partner=192.167.0.2",
+                @"Initial Catalog=<database_name>",
+                @"Integrated Security=True",
+                @"Load Balance Timeout=4",
+                @"Max Pool Size=6",
+                @"Min Pool Size=5",
+                @"MultipleActiveResultSets=True",
+                @"MultiSubnetFailover=True",
+                @"Packet Size=2048",
+                @"Password=""test""""'""",
+                @"Persist Security Info=True",
+                @"Pooling=True",
+                @"Replication=True",
+                @"Transaction Binding=""Explicit Unbind""",
+                @"TrustServerCertificate=True",
+                @"Type System Version=""SQL Server 2012""",
+                @"User ID=<user_id>",
+                @"User Instance=True",
+                @"Workstation ID=localhost"
+            };
+
+            Assert.IsTrue(values.SequenceEqual(expected));
+        }
+    }
+}
