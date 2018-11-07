92ca8c483e83398d944b72c7c47fffd34e06174d
rhrocha <rhrocha>
2018-11-07 14:58:44 -0200

    Coding pattern improvement for value casting of SQL Server bind parameters.

--- src/WDNUtils.DBSqlServer/Classes/DBSqlServerParameterInt32.cs
+++ src/WDNUtils.DBSqlServer/Classes/DBSqlServerParameterInt32.cs
@@ -30,76 +30,59 @@ namespace WDNUtils.DBSqlServer
             {
                 var value = GetValue();
 
-                if (value is null)
+                switch (value)
                 {
-                    return null;
-                }
-                else if (value is SqlInt32 sqlInt32)
-                {
-                    return sqlInt32.Value;
-                }
-                else if (value is SqlDecimal sqlDecimal)
-                {
-                    return checked((int)Math.Round(sqlDecimal.Value));
-                }
-                else if (value is SqlInt64 sqlInt64)
-                {
-                    return checked((int)sqlInt64.Value);
-                }
-                else if (value is SqlInt16 sqlInt16)
-                {
-                    return checked((int)sqlInt16.Value);
-                }
-                else if (value is SqlByte sqlByte)
-                {
-                    return checked((int)sqlByte.Value);
-                }
-                else if (value is SqlMoney sqlMoney)
-                {
-                    return checked((int)Math.Round(sqlMoney.Value));
-                }
-                else if (value is SqlDouble sqlDouble)
-                {
-                    return checked((int)Math.Round(sqlDouble.Value));
-                }
-                else if (value is SqlSingle sqlSingle)
-                {
-                    return checked((int)Math.Round(sqlSingle.Value));
-                }
-
-                switch (Type.GetTypeCode(value.GetType()))
-                {
-                    case TypeCode.UInt64:
-                        return checked((int)((ulong)value));
-                    case TypeCode.Int64:
-                        return checked((int)((long)value));
-                    case TypeCode.UInt32:
-                        return checked((int)((uint)value));
-                    case TypeCode.Int32:
-                        return (int)value;
-                    case TypeCode.UInt16:
-                        return checked((int)((ushort)value));
-                    case TypeCode.Int16:
-                        return checked((int)((short)value));
-                    case TypeCode.Byte:
-                        return checked((int)((byte)value));
-                    case TypeCode.SByte:
-                        return checked((int)((sbyte)value));
-                    case TypeCode.Decimal:
-                        return checked((int)Math.Round((decimal)value));
-                    case TypeCode.Single:
-                        return checked((int)Math.Round((float)value));
-                    case TypeCode.Double:
-                        return checked((int)Math.Round((double)value));
+                    case null:
+                        return null;
+                    case SqlInt32 sqlInt32:
+                        return sqlInt32.Value;
+                    case SqlDecimal sqlDecimal:
+                        return checked((int)Math.Round(sqlDecimal.Value));
+                    case SqlInt64 sqlInt64:
+                        return checked((int)sqlInt64.Value);
+                    case SqlInt16 sqlInt16:
+                        return checked((int)sqlInt16.Value);
+                    case SqlByte sqlByte:
+                        return checked((int)sqlByte.Value);
+                    case SqlMoney sqlMoney:
+                        return checked((int)Math.Round(sqlMoney.Value));
+                    case SqlDouble sqlDouble:
+                        return checked((int)Math.Round(sqlDouble.Value));
+                    case SqlSingle sqlSingle:
+                        return checked((int)Math.Round(sqlSingle.Value));
                     default:
-                        break;
+                        switch (Type.GetTypeCode(value.GetType()))
+                        {
+                            case TypeCode.UInt64:
+                                return checked((int)((ulong)value));
+                            case TypeCode.Int64:
+                                return checked((int)((long)value));
+                            case TypeCode.UInt32:
+                                return checked((int)((uint)value));
+                            case TypeCode.Int32:
+                                return (int)value;
+                            case TypeCode.UInt16:
+                                return checked((int)((ushort)value));
+                            case TypeCode.Int16:
+                                return checked((int)((short)value));
+                            case TypeCode.Byte:
+                                return checked((int)((byte)value));
+                            case TypeCode.SByte:
+                                return checked((int)((sbyte)value));
+                            case TypeCode.Decimal:
+                                return checked((int)Math.Round((decimal)value));
+                            case TypeCode.Single:
+                                return checked((int)Math.Round((float)value));
+                            case TypeCode.Double:
+                                return checked((int)Math.Round((double)value));
+                            default:
+                                throw new InvalidOperationException(string.Format(
+                                    DBSqlServerLocalizedText.DBSqlServerParameter_CastError,
+                                    Parameter.ParameterName,
+                                    value.GetType().FullName,
+                                    typeof(int).GetType().FullName));
+                        }
                 }
-
-                throw new InvalidOperationException(string.Format(
-                    DBSqlServerLocalizedText.DBSqlServerParameter_CastError,
-                    Parameter.ParameterName,
-                    value.GetType().FullName,
-                    typeof(int).GetType().FullName));
             }
 
             set
--- src/WDNUtils.DBSqlServer/Classes/DBSqlServerParameterInt64.cs
+++ src/WDNUtils.DBSqlServer/Classes/DBSqlServerParameterInt64.cs
@@ -30,76 +30,59 @@ namespace WDNUtils.DBSqlServer
             {
                 var value = GetValue();
 
-                if (value is null)
+                switch (value)
                 {
-                    return null;
-                }
-                else if (value is SqlInt64 sqlInt64)
-                {
-                    return sqlInt64.Value;
-                }
-                else if (value is SqlDecimal sqlDecimal)
-                {
-                    return checked((long)Math.Round(sqlDecimal.Value));
-                }
-                else if (value is SqlInt32 sqlInt32)
-                {
-                    return checked((long)sqlInt32.Value);
-                }
-                else if (value is SqlInt16 sqlInt16)
-                {
-                    return checked((long)sqlInt16.Value);
-                }
-                else if (value is SqlByte sqlByte)
-                {
-                    return checked((long)sqlByte.Value);
-                }
-                else if (value is SqlMoney sqlMoney)
-                {
-                    return checked((long)Math.Round(sqlMoney.Value));
-                }
-                else if (value is SqlDouble sqlDouble)
-                {
-                    return checked((long)Math.Round(sqlDouble.Value));
-                }
-                else if (value is SqlSingle sqlSingle)
-                {
-                    return checked((long)Math.Round(sqlSingle.Value));
-                }
-
-                switch (Type.GetTypeCode(value.GetType()))
-                {
-                    case TypeCode.UInt64:
-                        return checked((long)((ulong)value));
-                    case TypeCode.Int64:
-                        return (long)value;
-                    case TypeCode.UInt32:
-                        return checked((long)((uint)value));
-                    case TypeCode.Int32:
-                        return checked((long)((int)value));
-                    case TypeCode.UInt16:
-                        return checked((long)((ushort)value));
-                    case TypeCode.Int16:
-                        return checked((long)((short)value));
-                    case TypeCode.Byte:
-                        return checked((long)((byte)value));
-                    case TypeCode.SByte:
-                        return checked((long)((sbyte)value));
-                    case TypeCode.Decimal:
-                        return checked((long)Math.Round((decimal)value));
-                    case TypeCode.Single:
-                        return checked((long)Math.Round((float)value));
-                    case TypeCode.Double:
-                        return checked((long)Math.Round((double)value));
+                    case null:
+                        return null;
+                    case SqlInt64 sqlInt64:
+                        return sqlInt64.Value;
+                    case SqlDecimal sqlDecimal:
+                        return checked((long)Math.Round(sqlDecimal.Value));
+                    case SqlInt32 sqlInt32:
+                        return checked((long)sqlInt32.Value);
+                    case SqlInt16 sqlInt16:
+                        return checked((long)sqlInt16.Value);
+                    case SqlByte sqlByte:
+                        return checked((long)sqlByte.Value);
+                    case SqlMoney sqlMoney:
+                        return checked((long)Math.Round(sqlMoney.Value));
+                    case SqlDouble sqlDouble:
+                        return checked((long)Math.Round(sqlDouble.Value));
+                    case SqlSingle sqlSingle:
+                        return checked((long)Math.Round(sqlSingle.Value));
                     default:
-                        break;
+                        switch (Type.GetTypeCode(value.GetType()))
+                        {
+                            case TypeCode.UInt64:
+                                return checked((long)((ulong)value));
+                            case TypeCode.Int64:
+                                return (long)value;
+                            case TypeCode.UInt32:
+                                return checked((long)((uint)value));
+                            case TypeCode.Int32:
+                                return checked((long)((int)value));
+                            case TypeCode.UInt16:
+                                return checked((long)((ushort)value));
+                            case TypeCode.Int16:
+                                return checked((long)((short)value));
+                            case TypeCode.Byte:
+                                return checked((long)((byte)value));
+                            case TypeCode.SByte:
+                                return checked((long)((sbyte)value));
+                            case TypeCode.Decimal:
+                                return checked((long)Math.Round((decimal)value));
+                            case TypeCode.Single:
+                                return checked((long)Math.Round((float)value));
+                            case TypeCode.Double:
+                                return checked((long)Math.Round((double)value));
+                            default:
+                                throw new InvalidOperationException(string.Format(
+                                    DBSqlServerLocalizedText.DBSqlServerParameter_CastError,
+                                    Parameter.ParameterName,
+                                    value.GetType().FullName,
+                                    typeof(long).GetType().FullName));
+                        }
                 }
-
-                throw new InvalidOperationException(string.Format(
-                    DBSqlServerLocalizedText.DBSqlServerParameter_CastError,
-                    Parameter.ParameterName,
-                    value.GetType().FullName,
-                    typeof(long).GetType().FullName));
             }
 
             set
